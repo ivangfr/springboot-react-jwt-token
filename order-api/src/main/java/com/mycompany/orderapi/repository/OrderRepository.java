8016cb31da199ec47fca54b80c63f57eeec56a1f
Ivan Franchin <ivan.franchin@takeaway.com>
2022-05-06 13:50:01 +0200

    fix findByIdContainingOrDescriptionContainingOrderByCreatedAt; upgrade to mysql docker image 8.0.29

--- order-api/src/main/java/com/mycompany/orderapi/repository/OrderRepository.java
+++ order-api/src/main/java/com/mycompany/orderapi/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package com.mycompany.orderapi.repository;
 
 import com.mycompany.orderapi.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,7 @@ public interface OrderRepository extends JpaRepository<Order, String> {
 
     List<Order> findAllByOrderByCreatedAtDesc();
 
-    List<Order> findByIdContainingOrDescriptionContainingOrderByCreatedAt(String id, String description);
+    // Add @Param as a workaround for https://github.com/spring-projects/spring-data-jpa/issues/2512
+    // Remove them when this issue https://github.com/spring-projects/spring-data-jpa/issues/2519 is closed
+    List<Order> findByIdContainingOrDescriptionContainingOrderByCreatedAt(@Param("id") String id, @Param("description") String description);
 }
